c763aacab146eb9945c21898a962337916a4cfe7
Lungdjos <lungudjos@gmail.com>
2023-12-29 10:27:40 +0200

    clean-up

--- src/main/java/com/dms/document_management/managers/UserInfoManager.java
+++ src/main/java/com/dms/document_management/managers/UserInfoManager.java
@@ -3,9 +3,7 @@ package com.dms.document_management.managers;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
-import jakarta.transaction.Transactional;
-
 @Service
 public interface UserInfoManager extends UserDetailsService {
-    
+
 }
