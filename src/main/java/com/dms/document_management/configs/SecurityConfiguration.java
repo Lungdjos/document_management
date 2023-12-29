fbec714e834f7d50005ef7d6be37e0d8e82c2c72
Lungdjos <lungudjos@gmail.com>
2023-12-29 10:19:05 +0200

    creating a SecurityConfiguration class

---
+++ src/main/java/com/dms/document_management/configs/SecurityConfiguration.java
@@ -0,0 +1,64 @@
+package com.dms.document_management.configs;
+
+import com.dms.document_management.managers.implementation.user.UserInfoManagerImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    @Autowired
+    private JwtAuthentFilter jwtAuthentFilter;
+
+    @Bean
+    public UserDetailsService userDetailsService(){return new UserInfoManagerImpl();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable // Disable CSRF protection
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/dms/login").permitAll() // Permit access to /api/dms/login
+                        .requestMatchers("/api/dms/**").authenticated() // Require authentication for other /api/dms/**
+                )
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Set session creation policy to STATELESS
+                )
+                .authenticationProvider(authenticationProvider()) // Assuming you have an authenticationProvider bean
+                .addFilterBefore(jwtAuthentFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
