9446f31749e85f04f5678b528fe1614170c53312
Lungdjos <lungudjos@gmail.com>
2024-01-05 09:10:47 +0200

    refactoring and re-implementation

--- src/main/java/com/dms/document_management/managers/UserInfoManager.java
+++ src/main/java/com/dms/document_management/managers/UserInfoManager.java
@@ -1,10 +1,12 @@
 package com.dms.document_management.managers;
 
+import com.dms.document_management.dto.confis.AuthRequestDto;
+import com.dms.document_management.dto.confis.JwtResponseToken;
 import com.dms.document_management.dto.user.UserInfoDto;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 
 public interface UserInfoManager extends UserDetailsService {
-void createUser(UserInfoDto userInfoDto) throws Exception;
+    void createUser(UserInfoDto userInfoDto) throws Exception;
 }
--- src/main/java/com/dms/document_management/managers/implementation/user/UserInfoManagerImpl.java
+++ src/main/java/com/dms/document_management/managers/implementation/user/UserInfoManagerImpl.java
@@ -2,14 +2,18 @@ package com.dms.document_management.managers.implementation.user;
 
 import com.dms.document_management.dao.RolesDao;
 import com.dms.document_management.dao.UserInfoDao;
+import com.dms.document_management.dto.confis.AuthRequestDto;
+import com.dms.document_management.dto.confis.JwtResponseToken;
 import com.dms.document_management.dto.user.UserInfoDto;
+import com.dms.document_management.managers.RoleManager;
 import com.dms.document_management.models.roles.Role;
 import com.dms.document_management.models.user.UserInfo;
-import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
@@ -28,9 +32,8 @@ import java.util.Set;
 public class UserInfoManagerImpl implements UserInfoManager {
     @Autowired
     private UserInfoDao userInfoDao;
-
     @Autowired
-    private RolesDao rolesDao;
+    private RoleManager roleManager;
     @Bean
     public PasswordEncoder passwordEncoder(){return new BCryptPasswordEncoder();
     }
@@ -56,18 +59,20 @@ public class UserInfoManagerImpl implements UserInfoManager {
                 throw new Exception("User found err.");
             }
             UserInfo newUser = new UserInfo();
-            newUser.setFName(userInfoDto.getFName());
-            newUser.setLName(userInfoDto.getLName());
-            newUser.setMName(userInfoDto.getMName());
+            newUser.setFirstName(userInfoDto.getFirstName());
+            newUser.setLastName(userInfoDto.getLastName());
+            newUser.setMiddleName(userInfoDto.getMiddleName());
             newUser.setUsername(userInfoDto.getUsername());
             newUser.setEmail(userInfoDto.getEmail());
             newUser.setPassword(passwordEncoder().encode(userInfoDto.getPassword()));
-            Set<Role> roles = rolesDao.findByNameIn(userInfoDto.getRoles());;
+
+            Set<Role> roles = Collections.singleton(roleManager.getRole(userInfoDto.getRole()));
             newUser.setRoles(roles);
 
+            // save user
             userInfoDao.save(newUser);
         } catch (Exception e){
-            throw new RuntimeException();
+            throw new RuntimeException("Error creating a user ...", e);
         }
     }
 }
